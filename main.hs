8edcd595dc07b9e96f576c0f8356769cdb46c515
edugzlez <eduardo@brainum.es>
2021-01-24 19:52:02 +0100

    Expresiones regulares

--- main.hs
+++ main.hs
@@ -1,8 +1,48 @@
 import Automata
 
--- Expresiones regulares 'a' y 'b'
-a = RexChar 'a'
-b = RexChar 'b'
+deltax :: Char -> Status -> [Status]
+deltax 'a' (Q 0) = [Q 1]
+deltax 'a' (Q 1) = [Q 1]
+deltax 'b' (Q 1) = [Q 1]
+deltax 'a' (Q 2) = [Q 2]
+deltax 'b' (Q 2) = [Q 2, Q 3]
+deltax _ _ = []
+
+
+epsilon :: Status -> [Status]
+epsilon (Q 100) = [Q 0, Q 2]
+epsilon _ = []
+
+a = AFNe "ab" [Q 100, Q 0, Q 1, Q 2, Q 3] (Q 100) deltax [Q 1, Q 3] epsilon
+b = afnToafd (afneToafn a)
+c = reduce b
+d = normalizeNodes c
+(AFD vocab nodes initial delta terminals) = b
+(AFD vocab' nodes' initial' delta' terminals') = c
+(AFD vocab'' nodes'' initial'' delta'' terminals'') = d
+
+vocab1 = "ab"
+nodes1 = [Q 0, Q 1, Trash]
+initial1 = Q 0
+terminals1 = [Q 1]
+
+delta1 :: Char -> Status -> Status
+delta1 'a' (Q 0) = Q 1
+delta1 'a' (Q 1) = Q 1
+delta1 _ _ = Trash
+
+delta2 :: Char -> Status -> Status
+delta2 'b' (Q 0) = Q 1
+delta2 'b' (Q 1) = Q 1
+delta2 _ _ = Trash
+
+soloa = AFD vocab1 nodes1 initial1 delta1 terminals1
+solob = AFD vocab1 nodes1 initial1 delta2 terminals1
+
+orab = orAFD soloa solob
+
+andab = andAFD soloa solob
+
 
 -- Suma OR
 cualquiera = 'a' |+| 'b'
@@ -11,7 +51,13 @@ cualquiera = 'a' |+| 'b'
 todapalabra = (|^|) cualquiera
 
 -- Concatenación *
-terminaEnA = todapalabra |++| a
+terminaEnA :: Regex
+terminaEnA = todapalabra |++| (RexChar 'a')
 
 -- AFNe correspondiente
-afn = regToAFNe terminaEnA
+afne = regToAFNe terminaEnA
+afn = afneToafn afne
+
+afd = afnToafd afn
+
+afd2 = normalizeNodes $ reduce afd
